327297f0a5cafe0341d4622fa543bffe834cb35b
AdnaBumagat <adna.bumagat.up@phinmaed.com>
2023-12-05 14:02:47 +0800

    1st Commit and Push

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "AdrianJamesBumagat-Block6-Quiz"
+include(":app")
+ 
